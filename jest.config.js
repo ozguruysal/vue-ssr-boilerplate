eebe50b5b216b74db15ec4a77166fe4ef403adef
Özgür Uysal <ozguruysal@users.noreply.github.com>
2018-08-15 01:34:55 +0300

    Add tests with Vue Test Utils and Jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testURL: "http://localhost/",
+};
